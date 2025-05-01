3ee69ecb2787e29b3ec32d29763b41d7e8598c5b
t0nychopp3r <dennis.bilang@hotmail.com>
2025-05-01 18:11:51 +0200

    forgot to stage new init in document_processing folder

--- app/document_processing/__init__.py
+++ app/document_processing/__init__.py
@@ -4,5 +4,6 @@ Document processing package.
 
 from app.document_processing.pdf_loader import PDFLoader
 from app.document_processing.chunker import DocumentChunker
+from app.document_processing.layout_analysis import DocumentLayoutAnalyzer
 
-__all__ = ["PDFLoader", "DocumentChunker"] 
+__all__ = ["PDFLoader", "DocumentChunker", "DocumentLayoutAnalyzer"] 
